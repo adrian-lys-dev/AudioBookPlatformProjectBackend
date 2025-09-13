87dd704ccde7a598b93ead11c49227c0219f575c
adrian-lys-dev <drugakadrian@gmail.com>
2025-09-13 20:46:26 +0300

    db connection fix

--- Re_ABP_Backend/Extensions/DatabaseExtensions.cs
+++ Re_ABP_Backend/Extensions/DatabaseExtensions.cs
@@ -1,37 +1,18 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Azure.KeyVault;
-using Microsoft.Azure.Services.AppAuthentication;
-using Azure.Security.KeyVault.Secrets;
-using Azure.Identity;
-using Microsoft.Extensions.Configuration.AzureKeyVault;
 using Re_ABP_Backend.Data.DB;
 
-public static class DatabaseExtensions
+namespace Re_ABP_Backend.Extensions
 {
-    public static void AddDatabase(this IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
+    public static class DatabaseExtensions
     {
-        if (environment.IsProduction())
+        public static void AddDatabase(this IServiceCollection services, IConfiguration configuration, IHostEnvironment environment)
         {
-            var keyVaultURL = configuration.GetSection("KeyVault:KeyVaultURL");
-            var keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback));
-
-            // Use the AzureKeyVaultConfigurationExtensions.AddAzureKeyVault here
-            configuration = new ConfigurationBuilder()
-                .AddConfiguration(configuration)
-                .AddAzureKeyVault(keyVaultURL.Value!.ToString(), new DefaultKeyVaultSecretManager())
-                .Build();
-
-            var client = new SecretClient(new Uri(keyVaultURL.Value!.ToString()), new DefaultAzureCredential());
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            if (string.IsNullOrEmpty(connectionString))
+                throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
             services.AddDbContext<AppDBContext>(options =>
-            {
-                options.UseSqlServer(client.GetSecret("ProdConnection").Value.Value.ToString());
-            });
-        }
-        else if (environment.IsDevelopment())
-        {
-            services.AddDbContext<AppDBContext>(options =>
-                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+                options.UseSqlServer(connectionString));
         }
     }
 }
